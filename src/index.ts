512f16e00d186245f6614cb1754b4f9aace980b4
Núria Ramoneda <nuriarai@gmail.com>
2022-10-13 20:08:50 +0200

    Remove comments in index.ts to resolve Audit error of indention

--- src/index.ts
+++ src/index.ts
@@ -4,26 +4,3 @@ import Header from "./components/Header/Header.js";
 const appContainer: HTMLElement = document.querySelector(".app");
 const header = new Header(appContainer);
 header.render();
-
-/* Const parentElement = document.querySelector(".app");
-const newDiv = new Component(parentElement as HTMLElement, "maria", "ul");
-newDiv.render(); */
-
-/* const parentElement = document.querySelector(".app");
-const sectionS = new Component(
-  parentElement as HTMLElement,
-  "series",
-  "section"
-); */
-
-/* Const newSectionSeries = new SectionSeries(
-  sectionS.domElement,
-  "Pending series",
-  "You have 4 series pending to watch"
-);
-newSectionSeries.render(); */
-
-/* Series.forEach((series)) {
-  const serieCard = new cardSeries(pare, series);
-}
- */
